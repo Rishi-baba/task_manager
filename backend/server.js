18325943836d6124e4b984ee877a0a87044bf528
risibaba <rishibankar16@gmail.com>
2026-07-02 09:45:59 +0530

    Fix backend for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,10 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+}
+
+export default app;
